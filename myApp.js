2a3b994aeffebb9afcac36e036da7ff19f0564b6
Rogelio Servidor <rogelioservidor@192.168.254.106>
2025-05-23 18:24:09 +0800

    firt commit

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+app.use(helmet());
+app.use(helmet.hidePoweredBy());
+app.use(helmet.frameguard({action: 'deny'}));
+app.use(helmet.xssFilter());
+app.use(helmet.noSniff());
+app.use(helmet.ieNoOpen());
 
 
 
