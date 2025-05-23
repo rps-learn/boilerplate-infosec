c5b14bf91fd628546760b5a29181c0d6598386b6
Rogelio Servidor <rogelioservidor@192.168.254.106>
2025-05-23 19:54:45 +0800

    myApp.js update

--- myApp.js
+++ myApp.js
@@ -9,6 +9,31 @@ app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
 
+const timeInSeconds = ninetyDaysInSeconds = 90*24*60*60; 
+app.use(helmet.hsts({maxAge: timeInSeconds, force: true}));
+app.use(helmet.dnsPrefetchControl());
+app.use(helmet.noCache());
+app.use(helmet.contentSecurityPolicy({
+
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", 'trusted-cdn.com']
+  }
+
+}));
+
+app.use(helmet({
+  frameguard: {         // configure
+    action: 'deny'
+  },
+  contentSecurityPolicy: {    // enable and configure
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ['style.com'],
+    }
+  },
+  dnsPrefetchControl: false     // disable
+}))
 
 
 
